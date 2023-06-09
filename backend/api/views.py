f29d8f1beb5196f710956bf44feb5ac2feb9fbf5
Dridi Mahdi <mahdi.dridi@esprit.tn>
2023-06-10 00:46:07 +0200

    CustomSerializer

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,13 +4,14 @@ from rest_framework.decorators import api_view
 import json
 from django.forms.models import model_to_dict
 from products.models import Products
+from products.ProductSerializer import ProductSerializer
 
 @api_view(["GET"])
 def api_home(request, *args, **kwargs):
-     modeldata = Products.objects.all().order_by("?").first()
+     instance = Products.objects.all().order_by("?").first()
      data = {}
-     if (modeldata):
-         data = model_to_dict(modeldata)
-         #data = model_to_dict(modeldata,fields=['id', 'title'])
+     if (instance):
+         #data = model_to_dict(instance)
+         data = ProductSerializer(instance).data
          # instance -> py dict -> return JSON to my lient
      return Response(data)
